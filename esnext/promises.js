3198eddf32d8f7e8a94c1036ad64a96be29d461e
Julio Barreto <juliocezarjr@gmail.com>
2022-02-20 17:03:24 -0300

    Promises com exemplo

--- esnext/promises.js
+++ esnext/promises.js
@@ -6,7 +6,9 @@ function falarDepoisDe (segundos, frase) {
     })
 }
 
+
 falarDepoisDe(3, 'Que legal!')
-    .then((frase)=> frase.concat('!?!'))
+    .then(frase=> frase.concat('!?!'))
     .then(outraFrase => console.log(outraFrase))
     .catch(e=>console.log(e))
+
